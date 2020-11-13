d4a8b062f6fd354f7563070f21d4a0a6c802dd18
mhkawabata <marcy.kawabata@gmail.com>
2020-11-12 17:23:29 -0800

    camera system fixed

---
+++ Assets/Scripts/WASDControls.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDControls : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        ThirdPersonMoveScript.instance.wasdMove = true;
+    }
+}
