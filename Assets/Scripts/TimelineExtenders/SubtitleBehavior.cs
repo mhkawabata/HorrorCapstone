917a44cf71b6824858f8e69c5c44a3e7d49daa3d
mhkawabata <marcy.kawabata@gmail.com>
2021-01-26 17:27:27 -0800

    set up groundwork. come back later to integrate with existing dialogue system

---
+++ Assets/Scripts/TimelineExtenders/SubtitleBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Playables;
+
+public class SubtitleBehavior : PlayableBehaviour
+{
+    // Start is called before the first frame update
+    
+        public string subtitleText;
+
+  
+}
